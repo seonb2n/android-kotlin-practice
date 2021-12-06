96cbe058210e371fa56044770ba1599768329818
seonb2n <626ksb@gmail.com>
2021-12-06 21:19:37 +0900

    pjt 초기화

--- app/src/main/java/com/example/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -15,38 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val heightEditText: EditText = findViewById(R.id.editText_height)
-        val weightEditText = findViewById<EditText>(R.id.editText_weight)
-
-        val resultButton = findViewById<Button>(R.id.button_Result)
-        val resultTextView = findViewById<TextView>(R.id.textView_Result)
-
-        resultButton.setOnClickListener{
-            Log.d("MainActivity", "ResultButton is clicked")
-
-            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()) {
-                Toast.makeText(this, "Empty value", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            val height: Int = heightEditText.text.toString().toInt()
-            val weight: Int = weightEditText.text.toString().toInt()
-
-            Log.d("MainActivity", "height : $height weight : $weight")
-
-            val bmi = weight / (height / 100.0).pow(2.0)
-            val resulText = when {
-                bmi >= 35.0 -> "고도 비만"
-                bmi >= 30.0 -> "중정도 비만"
-                bmi >= 25.0 -> "경도 비만"
-                bmi >= 23.0 -> "과체중"
-                bmi >= 18.5 -> "정상체중"
-                else -> "저체중"
-            }
-            resultTextView.setText(resulText)
-
-        }
-
     }
 
 }
