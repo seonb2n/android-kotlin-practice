c2426d916def2cdf72e6c49ce8beab13281136bf
seonb2n <626ksb@gmail.com>
2021-12-10 11:20:02 +0900

    인증 화면 구현, 및 비밀번호 변경 기능 추가

--- app/src/main/java/com/example/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.kotlinapplication
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.pow
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,10 +36,83 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private val openButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.openButton)
+    }
+
+    private val changePasswordButton: AppCompatButton by lazy {
+        findViewById<AppCompatButton>(R.id.changePasswordButton)
+    }
+
+    private var changePasswordMode = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //호출하지 않으면 apply 에 적용이 안됨
+        numberPicker1
+        numberPicker2
+        numberPicker3
+
+        openButton.setOnClickListener {
+
+            if(changePasswordMode) {
+                Toast.makeText(this, "비밀번호 변경 중", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val passwordPreferences = getSharedPreferences("psssword", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+            if(passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                //패스워드 성공
+                //startActivity()
+            } else {
+                errorDialog()
+                //실패
+            }
+        }
+
+        changePasswordButton.setOnClickListener {
+
+            val passwordPreferences = getSharedPreferences("psssword", Context.MODE_PRIVATE)
+            val passwordFromUser = "${numberPicker1.value}${numberPicker2.value}${numberPicker3.value}"
+
+            if(changePasswordMode) {
+                //번호를 저장하는 기능
+                passwordPreferences.edit(true) {
+                    putString("password", passwordFromUser)
+                }
+
+                changePasswordMode = false
+                changePasswordButton.setBackgroundColor(Color.BLACK)
+
+            } else {
+                //change password mode 활성화
+
+                if(passwordPreferences.getString("password", "000").equals(passwordFromUser)) {
+                    changePasswordMode = true
+                    Toast.makeText(this, "변경할 패스워드를 입력해주세요", Toast.LENGTH_SHORT).show()
+                    changePasswordButton.setBackgroundColor(Color.RED)
+
+                } else {
+                    errorDialog()
+                    //실패
+                }
+
+            }
+
+        }
+
+    }
+
+    private fun errorDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("실패")
+            .setMessage("비밀번호가 잘못됐습니다")
+            .setPositiveButton("확인") { dialog, which -> }
+            .create()
+            .show()
     }
 
 }
