c7fbad333299da60928555f77ae1039beec72d6e
seonb2n <626ksb@gmail.com>
2021-12-11 15:46:27 +0900

    expression text view 설정

--- app/src/main/java/com/example/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.util.Log
 import android.view.View
 import android.widget.*
@@ -14,13 +17,97 @@ import androidx.core.content.edit
 
 class MainActivity : AppCompatActivity() {
 
+    private val expressionTextView: TextView by lazy {
+        findViewById<TextView>(R.id.expressionTextView)
+    }
+
+    private val resultTextView: TextView by lazy {
+        findViewById<TextView>(R.id.resultTextView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View) {
+    private var isOperator = false
+    private var hasOperator = false
 
+    fun buttonClicked(v: View) {
+        when(v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> operatorButtonClicked("+")
+            R.id.buttonMinus -> operatorButtonClicked("-")
+            R.id.buttonMulty -> operatorButtonClicked("*")
+            R.id.buttonDivide -> operatorButtonClicked("/")
+            R.id.buttonModulo -> operatorButtonClicked("%")
+        }
+    }
+
+    private fun numberButtonClicked(number: String) {
+
+        if (isOperator) {
+            expressionTextView.append(" ")
+        }
+
+        isOperator = false
+
+        val expressionText = expressionTextView.text.split(" ")
+        if (expressionText.isNotEmpty() && expressionText.last().length >= 15) {
+            Toast.makeText(this, "15 자리 까지만 사용할 수 있습니다", Toast.LENGTH_SHORT).show()
+            return
+        } else if (expressionText.last().isEmpty() && number == "0") {
+            Toast.makeText(this, "0은 제일 앞에 올 수 없습니다", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        expressionTextView.append(number)
+
+        //TODO 계산 결과 resultView 에 set
+    }
+
+    private fun operatorButtonClicked(operator: String) {
+        if (expressionTextView.text.isEmpty()) {
+            Toast.makeText(this, "연산자가 제일 앞에 올 수 없습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        when {
+            isOperator -> {
+                //방금 입력한 연산자가 존재
+                val text= expressionTextView.text.toString()
+                expressionTextView.text = text.dropLast(1) + operator
+            }
+            hasOperator -> {
+                //연산자가 1개 이미 존재
+                Toast.makeText(this, "연산자는 1개만 사용 가능합니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
+            else -> {
+                //연산자를 숫자 다음으로 처음 넣는 경우
+                expressionTextView.append(" $operator")
+            }
+        }
+
+        val ssb = SpannableStringBuilder(expressionTextView.text)
+        ssb.setSpan(
+            ForegroundColorSpan(getColor(R.color.green)),
+            expressionTextView.text.length -1,
+            expressionTextView.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        expressionTextView.text = ssb
+        isOperator = true
+        hasOperator = true
     }
 
     fun clearButtonClicked(v: View) {
