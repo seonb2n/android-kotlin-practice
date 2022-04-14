023d42b1ab5d1270f795058478f1a485e4e805f9
seonb2n <626ksb@gmail.com>
2022-04-14 21:31:33 +0900

    ui 구성

--- app/src/main/java/com/example/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -14,13 +14,30 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import androidx.viewpager2.widget.ViewPager2
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        viewPager.adapter = QuotesPagerAdapter(
+            listOf(
+                Quote(
+                    "나는 생각한다. 고로 나는 존재한다",
+                    "데카르트"
+                )
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/kotlinapplication/Quote.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinapplication
+
+data class Quote(
+    val quote: String,
+    val name: String
+)
