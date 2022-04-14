688b0d3e20ea35924aa067f6af6c91e8e034a2c2
seonb2n <626ksb@gmail.com>
2022-04-14 21:02:52 +0900

    pjt init

--- app/src/main/java/com/example/kotlinapplication/MainActivity.kt
+++ app/src/main/java/com/example/kotlinapplication/MainActivity.kt
@@ -18,27 +18,9 @@ import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
 
-    private val resultTextView: TextView by lazy{
-        findViewById(R.id.resultTextView)
-    }
-
-    private val firebaseToken: TextView by lazy {
-        findViewById(R.id.fireBaseTokenTextView)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initFireBase()
     }
-
-    private fun initFireBase() {
-        FirebaseMessaging.getInstance().token
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    firebaseToken.text = task.result
-                }
-            }
-    }
-
 }
